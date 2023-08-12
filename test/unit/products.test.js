39207b57f3b08337537a3b1979ef358dbe2f2001
subin <github@subin.kr>
2023-08-12 12:54:20 +0900

    add createProduct function test

---
+++ test/unit/products.test.js
@@ -0,0 +1,6 @@
+const productController = require("../../controller/products");
+describe("Product Controller Create", () => {
+  it("should have a createProduct function", () => {
+    expect(typeof productController.createProduct).toBe("function");
+  });
+});
