ba305d1c8c836dc4b5d2f8b121f555c64cb805b4
subin <github@subin.kr>
2023-08-12 15:11:13 +0900

    Make unit test

--- controller/products.js
+++ controller/products.js
@@ -1,4 +1,16 @@
 const productModel = require("../models/Product");
-exports.createProduct = (req, res, next) => {
-  productModel.create(req.body);
+
+exports.createProduct = async (req, res, next) => {
+  try {
+    // 외부 데이터베이스에서 데이터를 받아올 때는 async / await 꼭 사용
+    const craetedProduct = await productModel.create(req.body);
+    console.log("craetedProduct", craetedProduct);
+    // response statusCode를 201로 설정 후 빈 값 전송
+    //   res.status(201).send();
+    // response statusCode를 201로 설정 후 json으로 createdProduct 전송
+    res.status(201).json(craetedProduct);
+  } catch (error) {
+    // 오류 발생 시 실행
+    next(error);
+  }
 };
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -10,7 +10,7 @@ beforeEach(() => {
   // mock request, response 생성
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
-  next = null;
+  next = jest.fn();
 });
 describe("Product Controller Create", () => {
   beforeEach(() => {
@@ -18,14 +18,43 @@ describe("Product Controller Create", () => {
     // productModel.create시 mongoose.schema에 해당하는 req.body가 필요
     req.body = newProduct;
   });
+
   it("should have a createProduct function", () => {
     expect(typeof productController.createProduct).toBe("function");
   });
-  it("should call ProductModel.create", () => {
-    productController.createProduct(req, res, next);
+
+  it("should call ProductModel.create", async () => {
+    await productController.createProduct(req, res, next);
     // productModel.create가 호출되는지 확인
     // expect(productModel.create).toBeCalled();
     // productModel.create 호출 시 newProduct가 인자값에 들어가 있는지 확인
     expect(productModel.create).toBeCalledWith(newProduct);
+  });
+
+  it("should return 201 response code", async () => {
+    await productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    // res.status(201).send()에서 true가 발생했는지 확인
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return json body in response", async () => {
+    // mock function에 리턴값 지정
+    productModel.create.mockReturnValue(newProduct);
+    await productController.createProduct(req, res, next);
+    // res의 데이터가 json 형식의 newProduct인지 확인
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+  });
+
+  it("should handle errors", async () => {
+    // 에러 메세지 작성
+    const errorMessage = { message: "description property missing" };
+    // 에러 메세지 설정
+    const rejectedPromise = Promise.reject(errorMessage);
+    // 가짜 함수의 리턴값을 설정된 에러 메세지로 지정
+    productModel.create.mockReturnValue(rejectedPromise);
+    await productController.createProduct(req, res, next);
+    // next의 인자값으로 reject message가 들어가 호출되는지 확인
+    expect(next).toBeCalledWith(errorMessage);
   });
 });
